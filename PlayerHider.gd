8914433439e5ee14b3e764500e90ec24b336a9b7
Sacdeneu <sacdeneu@gmail.com>
2020-05-18 19:11:23 +0200

    fixed cone detection every frame

--- PlayerHider.gd
+++ PlayerHider.gd
@@ -4,6 +4,10 @@ var pos = Vector2()
 var direction = Vector2()
 
 func _ready():
+	set_physics_process(true)
+	
+func _physics_process(_delta):
 	var Player = get_tree().get_root().get_node("./Root/PlayerSeeker")
 	direction = (position - Player.position).normalized()
 	pos = position
+	update()
