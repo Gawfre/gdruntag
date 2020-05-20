50f88eb66a3e467ad0dc97df771ee6240cb7b23b
Sacdeneu <sacdeneu@gmail.com>
2020-05-20 20:16:21 +0200

    synchro for angle detection

--- PlayerSeeker.gd
+++ PlayerSeeker.gd
@@ -9,6 +9,8 @@ var draw_color = GREEN
 var ACCELERATIONSEEKER = 1000
 var MAX_SPEEDSEEKER = 500
 
+puppet var puppet_direction = Vector2()
+puppet var puppet_angle = 0
 
 func _init():
 	.ACCELERATION_set(ACCELERATIONSEEKER)
@@ -24,28 +26,38 @@ func _ready():
 	set_physics_process(true)
 	
 func _physics_process(_delta):
-	var pos = position
-	direction = (get_global_mouse_position() - pos).normalized()
-	angle = 90 - rad2deg(direction.angle())
-	
-	var detect_count = 0
-	for node in get_tree().get_nodes_in_group('detectable'):
-		if pos.distance_to(node.pos) < DETECT_RADIUS:
-			# Find the angle to the node, using the dot product
-			#var dot_product = direction.dot(node.direction)
-			var angle_to_node = rad2deg(direction.angle_to(node.direction))
-			#var angle_to_node = rad2deg(acos(dot_product))
-			if  abs(angle_to_node) < FOV/2:
-				detect_count +=1
-			
-			#If it's within the Player's cone of vision, the object is detected
-			
-	# DRAWING
-	if detect_count >0:
-		draw_color = RED
+	if is_network_master():
+		var pos = position
+		direction = (get_global_mouse_position() - pos).normalized()
+		angle = 90 - rad2deg(direction.angle())
+		
+		var detect_count = 0
+		for node in get_tree().get_nodes_in_group('detectable'):
+			if pos.distance_to(node.pos) < DETECT_RADIUS:
+				# Find the angle to the node, using the dot product
+				#var dot_product = direction.dot(node.direction)
+				var angle_to_node = rad2deg(direction.angle_to(node.direction))
+				#var angle_to_node = rad2deg(acos(dot_product))
+				if  abs(angle_to_node) < FOV/2:
+					detect_count +=1
+				
+				#If it's within the Player's cone of vision, the object is detected
+				
+		# DRAWING
+		if detect_count >0:
+			draw_color = RED
+		else:
+			draw_color = GREEN
+		update()
+		rset_unreliable("puppet_direction", direction)
+		rset_unreliable("puppet_angle", angle)
 	else:
-		draw_color = GREEN
-	update()
+		direction = puppet_direction
+		angle = puppet_angle
+	if not is_network_master():
+		direction = puppet_direction
+		angle = puppet_angle
+		update()
 
 func _draw():
 	draw_circle_arc_poly(Vector2(), DETECT_RADIUS,  angle - FOV/2, angle + FOV/2, draw_color)
