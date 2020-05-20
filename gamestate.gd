56572599acebcea83c39748e53ba253b024b4858
MarcRobin <>
2020-05-20 19:16:28 +0200

    camera follow player

--- gamestate.gd
+++ gamestate.gd
@@ -90,6 +90,7 @@ remote func pre_start_game(spawn_points):
 		if p_id == get_tree().get_network_unique_id():
 			# If node for this peer id, set name.
 			player.set_player_name(player_name)
+			player.get_node("Camera2D").make_current()
 		else:
 			# Otherwise set name from peer.
 			player.set_player_name(players[p_id])
