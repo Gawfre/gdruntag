ffc7164cb86fc8619e302868781b120e5b5d9aea
Sacdeneu <sacdeneu@gmail.com>
2020-05-27 02:53:10 +0200

    fix issues relative to light boost

--- CollectableLight.gd
+++ CollectableLight.gd
@@ -1,6 +1,6 @@
 extends "res://Collectable.gd"
 
-func _on_Collectable_body_entered(body):
+remotesync func _on_Collectable_body_entered(body):
 	if body.is_in_group("player"):
 		if(!body.get_light_boost_allowed()):
 			body.set_bool_light()
